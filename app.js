aeeff88bd384bc95e4263ec40ced6ed947ad27fe
ERASTUS-DOH <erastusdoh@gmail.com>
2021-10-12 19:45:15 +0000

    allDatapoints captured

--- app.js
+++ app.js
@@ -2,8 +2,44 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName:''
+
     };
   },
+  methods:{
+    updateName(){
+      this.name = "Welcome";
+      console.log("i have been called");
+    },
+    setConfirmedName(){
+      this.confirmedName = this.name;
+    },
+    subMethod(){
+      alert("form-submitted");
+    },
+    setNameValue(event, lastName){
+      this.name = event.target.value;
+    },
+    increaseCounter(incrementer){
+      this.counter += incrementer;
+    },
+    decreaseCounter(decrement){
+      this.counter -= decrement;
+    },
+    resetInput(){
+      this.name = '';
+    }
+  },
+  computed:{
+    outPutFullName() {
+      console.log('Running again')
+      if(this.name === ''){
+        return '';
+      }
+      return this.name + ' ' + 'Doh';
+    }
+  }
 });
 
 app.mount('#events');
