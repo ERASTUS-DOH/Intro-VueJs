83f568a4d3ce628035323e1676fbaba9e076341b
ERASTUS-DOH <erastusdoh@gmail.com>
2021-10-26 09:50:16 +0000

    finished basics of components

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      currentUserInput: '',
-      message: 'Vue is great!',
-    };
-  },
-  methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
-    },
-    setText() {
-      this.message = this.$refs.input.value;
-    },
-  },
+    data() {
+        return{
+            friends: [
+                {
+                    id: 'Elinam',
+                    name: 'Erastus Doh',
+                    telephone: '0241406244',
+                    email: 'mawumelina41@gmail.com'
+                }
+            ],
+        };
+    }
 });
 
-app.mount('#app');
+app.component('friend-contact', {
+    template:`
+        <li>
+           <h2>{{ friend.name }}</h2>
+             <button @click="toggleVisibility()"> {{  detailAreVisible ? 'Hide' : 'Show'}} Details</button>
+             <ul v-if="detailsAreVisible">
+               <li><strong>Phone:</strong> {{friend.telephone}}</li>
+               <li><strong>Email:</strong> {{friend.email}}</li>
+             </ul>
+        </li>`,
 
-//leave the world of js.
-let name = "Erastus";
-let fullName = name + " Doh";
-console.log(fullName);
-name = "Erastus Elinam";
-console.log(fullName);
-
-const data = {
-  details: "turtabl.io",
-  newWorkPlace: " ,Goldman Sachs"
-
-};
-
-const handler = {
-  set(target, key, value){
-    if(key === target.details){
-     console.log("Wow");
+    data(){
+        return{
+            detailsAreVisible: false,
+            friend: {
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                telephone: '0241406244',
+                email: 'erastusdoh@gmail.com'
+            },
+        };
+    },
+    methods:{
+        toggleVisibility() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
     }
-    target.details = target.newWorkPlace;
-  }
-};
 
-const proxy = new Proxy(data, handler);
-proxy.details = "Google";
-console.log(proxy.details);
+});
+app.mount('#app');
