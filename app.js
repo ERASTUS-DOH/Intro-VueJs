1749a4cc203228bccfc8f7ed5ee4dab1cfeaafcc
ERASTUS-DOH <erastusdoh@gmail.com>
2021-10-26 03:13:36 +0000

    understood some few behind the scences

--- app.js
+++ app.js
@@ -1,103 +1,44 @@
-function getRandomValue(min, max){
-  return Math.floor(Math.random() * (max - min)) + min ;
-}
-
 const app = Vue.createApp({
-  data(){
+  data() {
     return {
-      playerHealth: 100,
-      monsterHealth: 100,
-      currentRound: 0,
-      winner: null,
-      logMessage: []
-    }
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
   },
-  watch:{
-    playerHealth(value){
-      if(value <= 0 && this.monsterHealth <= 0){
-        // We have got a draw.
-        this.winner = 'draw';
-      }else if (value <= 0 ){
-        // We lost.
-        this.winner = 'monster'
-      }
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
     },
-    monsterHealth(value){
-      if(value <= 0 && this.playerHealth <= 0){
-        //both monster and player drew.
-        this.winner = 'draw';
-      }else if (value <= 0){
-        //monster lost and player won.
-        this.winner = 'player';
-      }
-    }
+    setText() {
+      this.message = this.$refs.input.value;
+    },
   },
-  computed:{
-    monsterHealthBarStyles(){
-      if(this.monsterHealth <= 0){
-        return {width: 0 + '%'}
-      }
-      return {width: this.monsterHealth + '%'}
-    },
-    playerHealthBarStyles(){
-      if(this.playerHealth <= 0){
-        return {width: 0 + '%'}
-      }
-      return {width: this.playerHealth + '%'}
-    },
-    specialAttackBottonAvailable(){
-      return (this.currentRound % 3) !== 0;
-    }
-  },
-  methods:{
-    startGame(){
-      this.playerHealth = 100;
-      this.monsterHealth = 100;
-      this.winner = null;
-      this.currentRound = 0;
-      this.logMessage = [];
-    },
-    attackMonster(){
-      this.currentRound++;
-      const attackValue = getRandomValue(5,12);
-      this.monsterHealth -= attackValue;
-      this.attackPlayer();
-      this.addLogMessage('player', 'attack', attackValue);
-    },
-    attackPlayer(){
-      const attackValue = getRandomValue(8,12);
-      this.playerHealth -= attackValue;
-      this.addLogMessage('monster', 'attack', attackValue);
-    },
-    specialAttackMonster(){
-      this.currentRound++;
-      const attackValue = getRandomValue(10,25);
-      this.monsterHealth -= attackValue;
-      this.attackPlayer();
-      this.addLogMessage('player','special-attack', attackValue);
-    },
-    healPlayer(){
-      this.currentRound++;
-      const healValue = getRandomValue(8,20);
-      if((this.playerHealth + healValue) > 100){
-        this.playerHealth = 100;
-      }else{
-        this.playerHealth += healValue;
-      }
-      this.attackPlayer();
-      this.addLogMessage('player','heal', healValue)
-    },
-    surrender(){
-      this.winner = 'monster';
-    },
-    addLogMessage(who, what, value){
-      this.logMessage.unshift({
-        actionBy:who,
-        actionType: what,
-        actionValue: value
-      })
-    },
-  }
 });
 
-app.mount('#game');
+app.mount('#app');
+
+//leave the world of js.
+let name = "Erastus";
+let fullName = name + " Doh";
+console.log(fullName);
+name = "Erastus Elinam";
+console.log(fullName);
+
+const data = {
+  details: "turtabl.io",
+  newWorkPlace: " ,Goldman Sachs"
+
+};
+
+const handler = {
+  set(target, key, value){
+    if(key === target.details){
+     console.log("Wow");
+    }
+    target.details = target.newWorkPlace;
+  }
+};
+
+const proxy = new Proxy(data, handler);
+proxy.details = "Google";
+console.log(proxy.details);
